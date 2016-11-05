e33805aea99b44ddfb23d045497939b7960ad3c7
newmri <newmri@naver.com>
2016-11-06 00:00:44 +0900

    a

---
+++ HA Project/Battle.py
@@ -0,0 +1,130 @@
+from CharClass import *
+from map import *
+
+# This is test.
+
+
+def handle_events(man,map):
+    events=get_events()
+    for event in events:
+        if event.type==SDL_QUIT:
+            close_canvas()
+        else:
+            if event.type==SDL_KEYDOWN:
+                if event.key==SDLK_ESCAPE:
+                    close_canvas()
+                elif man.fightRun==True:
+                    if event.key == SDLK_SPACE:
+                        print("skill")
+                        skillEfect = load_image('Resources\\SkillEffect\\Fire.png')
+                        hitEffect = [load_image('Resources\\BloodEffect\\bloodtrail0.png'),load_image('Resources\\BloodEffect\\bloodtrail_1.png'),
+                                     load_image('Resources\\BloodEffect\\bloodtrail_2.png'),load_image('Resources\\BloodEffect\\bloodtrail_3.png')
+                                     ,load_image('Resources\\BloodEffect\\bloodtrail_4.png'),load_image('Resources\\BloodEffect\\bloodtrail_5.png')
+                                     ,load_image('Resources\\BloodEffect\\bloodtrail_6.png'),load_image('Resources\\BloodEffect\\bloodtrail_7.png')]
+
+                        man.setSkillEfect(skillEfect)
+                        map.monster.setHitEffect(hitEffect)
+                        man.drawSkillEfect(man,map)
+                        map.monster.calDamage(man.skillDmg)
+                        print(map.monster.hp)
+                        #man.myTurn=False
+
+                elif man.fight==True:
+                     if event.key == SDLK_SPACE:
+                        if man.skinType=='Original':
+                            skill=load_image('Resources\\SelectOfSkills\\FireBallSel.png')
+                            man.setSkill(skill)
+                            man.fightRun=True
+                            man.resetBattleDialog()
+                            clear_canvas()
+                            map.draw()
+                            man.drawBattle()
+                            man.myTurn = False
+
+
+                     if event.key == SDLK_UP:
+                        man.fight=True
+                     if event.key==SDLK_DOWN:
+                         man.fight=False
+                elif man.fight==False:
+                      if event.key==SDLK_SPACE:
+                          man.fight = 'outhouse'
+                      if event.key==SDLK_UP:
+                          man.fight = True
+                      if event.key==SDLK_DOWN:
+                          man.fight = False
+                elif event.key == SDLK_RIGHT:
+                      pass
+                elif event.key == SDLK_LEFT:
+                      pass
+
+
+
+def battle(man,map):
+
+    clear_canvas()
+    skin = load_image('Resources\\Monster\\rocket.png')
+    rocket = Monster(skin, 'Original')
+    map.setMonster(rocket)
+    map.monster.x = map.x-30
+    map.monster.y = map.y / 2
+    map.draw()
+    man.x=30
+    man.y=map.y/2
+    man.drawBattle()
+    dialogFight = load_image('Resources\\DialogBox\\DialogFight.png')
+    dialogRunAway = load_image('Resources\\DialogBox\\DialogRunAway.png')
+    man.setBattleDialog(dialogFight)
+    man.drawBattleDialog()
+    man.update()
+    onBattle=True
+    while(onBattle==True):
+        handle_events(man,map)
+        get_events()
+        if man.fight=='outhouse':
+            man.fight=True
+            return 'outhouse',man
+        elif man.fight==True:
+            if man.fightRun==False:
+                man.setBattleDialog(dialogFight)
+                if man.drawDiaChk==True:
+                   man.drawBattleDialog()
+
+            else:
+                man.drawSkill_Box(man.y)
+        elif man.fight==False:
+            if man.fightRun == False:
+               man.setBattleDialog(dialogRunAway)
+               if man.drawDiaChk == True:
+                   man.drawBattleDialog()
+            else:
+                man.drawSkill_Box(man.y)
+        man.update()
+        map.monster.myTurn=True
+        if map.monster.myTurn==True:
+            skillEfect = [load_image('Resources\\SkillEffect\\Thunder1.png'),
+                      load_image('Resources\\SkillEffect\\Thunder2.png'),
+                      load_image('Resources\\SkillEffect\\Thunder3.png'),
+                      load_image('Resources\\SkillEffect\\Thunder4.png'),
+                      load_image('Resources\\SkillEffect\\Thunder5.png'),
+                      load_image('Resources\\SkillEffect\\Thunder6.png')
+                      ]
+
+            hitEffect = [load_image('Resources\\BloodEffect\\bloodtrail0.png'),
+                     load_image('Resources\\BloodEffect\\bloodtrail_1.png'),
+                     load_image('Resources\\BloodEffect\\bloodtrail_2.png'),
+                     load_image('Resources\\BloodEffect\\bloodtrail_3.png')
+            , load_image('Resources\\BloodEffect\\bloodtrail_4.png'),
+                     load_image('Resources\\BloodEffect\\bloodtrail_5.png')
+            , load_image('Resources\\BloodEffect\\bloodtrail_6.png'),
+                     load_image('Resources\\BloodEffect\\bloodtrail_7.png')]
+
+            map.monster.setSkillEfect(skillEfect)
+            man.setHitEffect(hitEffect)
+            map.monster.drawSkillEfect(man)
+            man.calDamage(map.monster.skillDmg)
+            man.myTurn=True
+            map.monster.myTurn == False
+
+
+
